0846095653cc2f5645eb2ace555496eb499874f2
Eduardo Mendez <8771854+PacoMdz@users.noreply.github.com>
2018-08-21 23:19:51 -0500

    Update PropertyErrorRule.cs

--- PropertyErrorRule.cs
+++ PropertyErrorRule.cs
@@ -1,16 +1,30 @@
 using System;
 
-namespace Mx.CustomNotifyDataError
+namespace Mx.Models.NotifyDataErrors
 {
-    public struct PropertyErrorRule
-    {
-        public PropertyErrorRule(Func<bool> failWith, string message)
-        {
-            FailWith = failWith;
-            Message = message;
-        }
+	/// <summary>
+	/// Define a rule for property validation.
+	/// </summary>
+	public struct PropertyErrorRule
+	{
+		/// <summary>
+		/// Define a rule for property validation, with a message.
+		/// </summary>
+		/// <param name="failWith">Rule condition to test the property value</param>
+		/// <param name="message">Message to show when the rule fails</param>
+		public PropertyErrorRule(Func<bool> failWith, string message)
+		{
+			FailWith = failWith;
+			Message = message;
+		}
 
-        public Func<bool> FailWith { get; private set; }
-        public string Message { get; private set; }
-    }
+		/// <summary>
+		/// Rule condition to test the property value.
+		/// </summary>
+		public Func<bool> FailWith { get; }
+		/// <summary>
+		/// Message to show when the rule fails.
+		/// </summary>
+		public string Message { get; }
+	}
 }
